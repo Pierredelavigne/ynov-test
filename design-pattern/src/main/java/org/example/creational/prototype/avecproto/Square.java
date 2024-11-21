a770927ee39d9efaced61f7ff1fb78495086166b
Pierredelavigne <72016192+Pierredelavigne@users.noreply.github.com>
2024-11-21 15:23:15 +0100

    Update Square.java

--- design-pattern/src/main/java/org/example/creational/prototype/avecproto/Square.java
+++ design-pattern/src/main/java/org/example/creational/prototype/avecproto/Square.java
@@ -2,9 +2,24 @@ package org.example.creational.prototype.avecproto;
 
 public class Square implements ShapePrototype{
 
+    private String color;
+    private int side;
+
+    public Square(String color, int side) {
+        this.color = color;
+        this.side = side;
+    }
 
     @Override
     public ShapePrototype clone() {
-        return null;
+        return new Square(color, side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "color='" + color + '\'' +
+                ", side=" + side +
+                '}';
     }
 }
