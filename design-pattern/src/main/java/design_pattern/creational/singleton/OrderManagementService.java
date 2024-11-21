8a26422f0d6eabd4c99be227d6e8f77f284ea273
Pierredelavigne <72016192+Pierredelavigne@users.noreply.github.com>
2024-11-21 09:42:41 +0100

    [+]Tp

--- design-pattern/src/main/java/design_pattern/creational/singleton/OrderManagementService.java
+++ design-pattern/src/main/java/design_pattern/creational/singleton/OrderManagementService.java
@@ -1,3 +1,19 @@
 package main.java.design_pattern.creational.singleton;
 
+public class OrderManagementService {
 
+    private static OrderManagementService instance;
+
+    private OrderManagementService() {}
+
+    public static OrderManagementService getInstance() {
+        if (instance == null) {
+            instance = new OrderManagementService();
+        }
+        return instance;
+    }
+
+
+
+
+}
